d71eec3f42e04c4e774a58e7501886eca862fc13
Sungjin-Wie <tmdwls1203@gmail.com>
2020-07-06 11:52:33 +0900

    [Add] redux module

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { HashRouter as Router, Switch } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,6 +21,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-};
+}
 
 export default App;
